cac4bf3397362a477e5eb156f35b891780d00e38
guanghua.zhu <guanghua.zhu@dianping.com>
2014-02-19 00:54:14 +0800

    awk

--- rename.sh
+++ rename.sh
@@ -1,2 +1,8 @@
-for i in *.JPG; do echo $i; done
-for i in *-doc-*.txt; do mv $i ${i/*-doc-/doc-}; done
+# for i in *.JPG; do echo $i; done
+cd /Volumes/bak/weiYun/red
+pwd
+# for i in *.mp3; do mv $i ${i/LRTS#/}; done
+# for i in *.mp3; do echo $i; done
+ls -1 | awk -F '[\#|\.]' '{print "mv " $0 " " $3 "." $1 "." $4}' | /bin/bash
+#ls -1 | awk -F '[\#|\.]' '{if(length($1)==1)print "mv " $0 " 00" $0}' | /bin/bash
+ls -1
